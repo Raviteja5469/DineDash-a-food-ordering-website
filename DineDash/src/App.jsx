5c75e1e7981ca83c44b2bb9fc9c8f9fab85f7cab
Raviteja5469 <ravitejaseguri@gmail.com>
2024-12-06 02:57:54 +0530

    a mior update at home page

--- DineDash/src/App.jsx
+++ DineDash/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar/navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className='app'>
       <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<cart />} />
+        <Route path="/order" element={<placeorder />} />
+      </Routes>
     </div>
   )
 }
