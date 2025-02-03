b64397eba81f53aaf6707faacb7030bdf5455cbd
Raviteja5469 <ravitejaseguri@gmail.com>
2025-02-04 02:43:56 +0530

    Updated the admin Page

--- DineDash/backend/controllers/foodController.js
+++ DineDash/backend/controllers/foodController.js
@@ -3,25 +3,27 @@ import fs from 'fs';
 
 // Add food
 const addFood = async (req, res) => {
-
   let image_filename = `${req.file.filename}`;
 
   const food = new foodModel({
     name: req.body.name,
-    description:req.body.description,
-    price: req.body.price, 
-    category: req.body.category, 
-    image: image_filename 
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename
+  });
 
-  })
+  // Create a data string for logging
+  const dataString = `Food Item: ${food.name}, Description: ${food.description}, Price: ${food.price}, Category: ${food.category}, Image: ${food.image}`;
+
   try {
     await food.save();
-    res.json({success:true,message:"Food Added"})
+    console.log(dataString); // Log the data string
+    res.json({ success: true, message: "Food Added" });
   } catch (error) {
-    console.log(error)
-    res.json({success:false,message:"Failed to add food"})
+    console.error(error);
+    res.json({ success: false, message: "Failed to add food" });
   }
 };
 
 export { addFood };
-
